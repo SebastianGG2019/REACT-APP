8d2c87c13152d360155433cc02dedb4b26177e47
Sebastian <Sebasguz.g12@gmail.com>
2021-06-11 12:34:22 -0500

    vers uno
    vers uno

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import NavBar from './components/NavBar';
+import Form from './components/Form'
+import LightBox from './components/LightBox';
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Form/>
+      <LightBox/>
+    </>
+  );
+}
+
+export default App;
